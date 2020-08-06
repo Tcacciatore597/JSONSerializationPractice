cc985af2260bed407b57a0338398316383c4a66b
Spencer Curtis <spencercolecurtis@gmail.com>
2020-08-06 09:21:24 -0600

    Initial Commit

---
+++ CodablePracticeiOS18/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodablePracticeiOS18
+//
+//  Created by Spencer Curtis on 8/6/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
